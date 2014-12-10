c807d2b1b158f353e0b7c3885e68538b9cf7f050
Jeremy <jeremyw@stashedboxes.com>
2014-12-09 22:01:23 -0800

    Update random.php
    generify

--- random.php
+++ random.php
@@ -14,9 +14,10 @@ if (!$_SESSION[_member] && !$winner) {
 		?>Please <a href="/login">Login</a> or <a href="/signup">Sign up</a> to select a winner.<? 
 		exit();
 		}
-?>The winner is of this contest chosen at random by a registered member of NSB from all the entries on instagram.<br />You do not have to be a registered member of NSB to win.<br />
+?>The winner is of this contest chosen at random by a registered member from all the entries on instagram.<br />
+You do not have to be a registered member to win.<br />
  We do this to make the possibility of being selected based on two factors:<br /><br />
-1) the server (NSB) randomly selecting an entry<br /> 2) the randomness of a member activating the script.<br /><br /><?
+1) the server randomly selecting an entry<br /> 2) the randomness of a user activating the script.<br /><br /><?
 if ($winner) { echo "<b>Winner</b> - <a href='$winner'>". $winner."</a>"; } else {
 
 	$sql = mysql_query("SELECT * FROM  `wp_live` WHERE  `blog_id` = $blog_id AND user_id NOT IN (6435181, 19289771, 216429479, 228081701) ORDER BY RAND() LIMIT 1"); // excludes us and shops from winning
